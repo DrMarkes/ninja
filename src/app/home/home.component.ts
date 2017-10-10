51c4a73597324593dfe48712d3fa2214e9fbfd38
Андрей Марков <user.name>
2017-10-10 19:29:46 +0700

    complete tutorial#13

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   homeTitle = 'Welcome to the Ninja!';
-  myString = 'I love chicken!';
-  myBoolean = true;
+  ninja = {
+    name: 'Yoshi',
+    belt: 'Black'
+  };
 
   constructor() { }
 
